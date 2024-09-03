51979c4b0659396c53ef8affa52ef638b60e4c81
erycaaaaa <eryca847@gmail.com>
2024-09-03 20:43:59 +0700

    Updated

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-    const elements = document.querySelectorAll('.scroll-animation');
-    elements.forEach(element => {
-      const distanceToTop = element.getBoundingClientRect().top;
-      if (distanceToTop < window.innerHeight) {
-        element.classList.add('show');
-      }
-    });
-  });
